7cdde74eca991b884f4b1bb6527f1d4939d0014e
matthewcory1 <matthewcory1@gmail.com>
2024-10-17 20:05:30 +0000

    Add laws for Semigroup and Monoid typeclasses.

---
+++ wildcat-laws/src/main/java/wildcat/laws/typeclasses/core/MonoidLaws.java
@@ -0,0 +1,20 @@
+package wildcat.laws.typeclasses.core;
+
+import org.assertj.core.api.Assertions;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Property;
+import wildcat.typeclasses.core.Monoid;
+
+public interface MonoidLaws<T extends @NonNull Object> extends SemigroupLaws<T> {
+    Monoid<T> instance();
+
+    @Property
+    default void identity(final @ForAll T a) {
+        final Monoid<T> monoid = instance();
+
+        Assertions.assertThat(monoid.combine(monoid.empty(), a)).isEqualTo(a);
+        Assertions.assertThat(monoid.combine(a, monoid.empty())).isEqualTo(a);
+    }
+}
