fe187c58b1aaf60bd0c43916075f6669fdfe0bb2
matthewcory1 <matthewcory1@gmail.com>
2024-11-04 23:23:54 +0000

    refactor(wildcat.laws.FunctorLaws): improve type safety
    Improved the type safety of `FunctorLaws` by:
    
    - Specifying more precise type bounds for the `unit` and `verifyEquals` methods.
    - Using `@NonNull` annotations to ensure non-null values are passed.
    
    These changes enhance the clarity and correctness of the `FunctorLaws` interface, making it easier to reason about and use.

--- wildcat-laws/src/main/java/wildcat/laws/typeclasses/core/FunctorLaws.java
+++ wildcat-laws/src/main/java/wildcat/laws/typeclasses/core/FunctorLaws.java
@@ -1,42 +1,40 @@
 package wildcat.laws.typeclasses.core;
 
 import java.util.function.Function;
-
-import org.checkerframework.checker.nullness.qual.NonNull;
-
 import net.jqwik.api.ForAll;
 import net.jqwik.api.Property;
+import org.checkerframework.checker.nullness.qual.NonNull;
 import wildcat.hkt.Kind;
 import wildcat.typeclasses.core.Functor;
 
 public interface FunctorLaws<For extends Functor.k, T extends @NonNull Object> {
-    Functor<For> instance();
-
-    Kind<For, T> unit(final T a);
-
-    <A extends @NonNull Object> void verifyEquals(final Kind<For, ? extends A> a, final Kind<For, ? extends A> b);
-
-    @Property
-    default void mapIdentity(final @ForAll T a) {
-        final Functor<For> functor = instance();
-        final Kind<For, T> unit = unit(a);
-
-        final Kind<For, ? extends T> mapped = functor.map(unit, x -> x);
-        verifyEquals(unit, mapped);
-    }
-
-    @Property
-    default void composition(final @ForAll T a, final @ForAll Function<? super T, ? extends String> f) {
-        final Functor<For> functor = instance();
-        final Kind<For, T> unit = unit(a);
-        final Function<String, Integer> len = String::length;
-
-        final Kind<For, ? extends String> mapped = functor.map(unit, f);
-
-        final Kind<For, ? extends Integer> mappedLen = functor.map(mapped, len);
-
-        final Kind<For, ? extends Integer> composed = functor.map(unit, f.andThen(len));
-
-        verifyEquals(mappedLen, composed);
-    }
+  Functor<For> instance();
+  
+  <U extends @NonNull Object> Kind<For, ? extends U> unit(final U a);
+  
+  <A extends @NonNull Object> void verifyEquals(final Kind<For, ? extends A> a, final Kind<For, ? extends A> b);
+  
+  @Property
+  default void functorMapIdentity(final @ForAll T a) {
+    final Functor<For> functor = instance();
+    final Kind<For, ? extends T> unit = unit(a);
+    
+    final Kind<For, ? extends T> mapped = functor.map(unit, Function.identity());
+    verifyEquals(unit, mapped);
+  }
+  
+  @Property
+  default void functorComposition(final @ForAll T a, final @ForAll Function<? super T, ? extends String> f) {
+    final Functor<For> functor = instance();
+    final Kind<For, ? extends T> unit = unit(a);
+    final Function<String, Integer> len = String::length;
+    
+    final Kind<For, ? extends String> mapped = functor.map(unit, f);
+    
+    final Kind<For, ? extends Integer> mappedLen = functor.map(mapped, len);
+    
+    final Kind<For, ? extends Integer> composed = functor.map(unit, f.andThen(len));
+    
+    verifyEquals(mappedLen, composed);
+  }
 }
