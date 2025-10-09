298e265f5cbdda3b0bc9cfc3c581d5a07eb8fa23
Matthew Cory <matthewcory1@gmail.com>
2025-10-09 22:00:02 +0000

    build: Upgrade to Java 21

--- build-logic/src/main/kotlin/common.gradle.kts
+++ build-logic/src/main/kotlin/common.gradle.kts
@@ -16,7 +16,7 @@ repositories {
 // Apply a specific Java toolchain to ease working on different environments.
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
+        languageVersion = JavaLanguageVersion.of(21)
     }
 }
 
