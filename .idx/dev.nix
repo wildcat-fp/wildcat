1d54e2773f0bd907099d54e6ebbc76c30de42f8a
Matthew Cory <matthewcory1@gmail.com>
2026-02-11 22:18:21 +0000

    Configure gpg stuff.

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -7,13 +7,15 @@
   # Use https://search.nixos.org/packages to find packages
   packages = [
     pkgs.python3
-    
+
     pkgs.zulu
     pkgs.zsh
-    
+
     pkgs.bat
     pkgs.tree
     pkgs.gh
+    pkgs.gnupg
+    pkgs.pinentry-curses
 
     pkgs.chezmoi
     pkgs.util-linux
@@ -23,7 +25,7 @@
   env = {
     ZSH = "${pkgs.oh-my-zsh}/share/oh-my-zsh";
   };
-  
+
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
@@ -44,29 +46,5 @@
       "zjffun.snippetsmanager"
     ];
 
-    # Enable previews
-    previews = {
-      enable = true;
-      previews = {
-        web = {
-          command = ["python3" "-m" "http.server" "$PORT" "--bind" "0.0.0.0"];
-          manager = "web";
-        };
-      };
-    };
-
-    # Workspace lifecycle hooks
-    workspace = {
-      # Runs when a workspace is first created
-      onCreate = {
-        # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
-      };
-      # Runs when the workspace is (re)started
-      onStart = {
-        # Example: start a background task to watch and re-build backend code
-        # watch-backend = "npm run watch-backend";
-      };
-    };
   };
 }
