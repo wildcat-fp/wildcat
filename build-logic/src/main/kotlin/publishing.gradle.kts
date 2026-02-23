8c4ecb3c89dc41d4b969455e1e806e3237a2455e
Matthew Cory <matthewcory1@gmail.com>
2026-02-23 20:31:11 +0000

    feat(release): Configure publishing and CI

--- build-logic/src/main/kotlin/publishing.gradle.kts
+++ build-logic/src/main/kotlin/publishing.gradle.kts
@@ -1,70 +1,46 @@
 
 plugins {
-  `maven-publish`
-  signing
+  id("com.vanniktech.maven.publish")
+  `java-library`
 }
 
-// Configure publishing for all sub-modules
-publishing {
-    publications {
-        register<MavenPublication>("mavenJava") {
-            from(components["java"])
-
-            artifact(tasks.getByName("sourcesJar"))
-
-            artifact(tasks.getByName("javadocJar")) {
-                classifier = "javadoc"
-            }
-
-
-            // Add extra metadata to the POM
-            pom {
-                name.set("Wildcat")
-                description.set("A functional programming library for Java")
-                url.set("https://github.com/wildcat-fp/wildcat")
-
-                // Configure licensing information
-                licenses {
-                    license {
-                        name.set("MIT")
-                        url.set("https://opensource.org/licenses/MIT")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("SkittishSloth")
-                        name.set("Matthew Cory")
-                        email.set("matthewcory1@gmail.com")
-                        url.set("https://github.com/SkittishSloth")
-                    }
-                }
-
-                scm {
-                    connection.set("scm:git:git://github.com/wildcat-fp/wildcat.git")
-                    developerConnection.set("scm:git:ssh://github.com/wildcat-fp/wildcat.git")
-                    url.set("https://github.com/wildcat-fp/wildcat")
-                }
-                issueManagement {
-                    system.set("GitHub")
-                    url.set("https://github.com/wildcat-fp/wildcat/issues")
-                }
-            }
-        }
-    }
+java {
+  withJavadocJar()
+  withSourcesJar()
 }
 
-// Configure signing for all publications
-signing {
-    // Signing is required for release builds, but not for snapshots
-    isRequired = !(project.version as String).endsWith("-SNAPSHOT")
+mavenPublishing {
+  val pomName = project.name
+  val pomDescription = project.description ?: "A functional programming library for Java"
 
-    // If the GPG key is present, configure it for signing.
-    // The build will fail if isRequired=true and the key is not present.
-    val gpgKey = System.getenv("GPG_SIGNING_KEY")
-    val gpgPassword = System.getenv("GPG_SIGNING_PASSWORD")
-    if (gpgKey != null) {
-        useInMemoryPgpKeys(gpgKey, gpgPassword)
+  coordinates("io.github.wildcat.fp", project.name, project.version.toString())
+
+  pom {
+    name.set(pomName)
+    description.set(pomDescription)
+    inceptionYear.set("2024")
+    url.set("https://github.com/wildcat-fp/wildcat")
+
+    licenses {
+      license {
+        name.set("MIT License")
+        url.set("https://opensource.org/licenses/MIT")
+        distribution.set("repo")
+      }
     }
 
-    sign(publishing.publications)
+    developers {
+      developer {
+        id.set("wildcat-fp")
+        name.set("Wildcat FP")
+        email.set("wildcat.fp@example.com")
+      }
+    }
+
+    scm {
+      url.set("https://github.com/wildcat-fp/wildcat")
+      connection.set("scm:git:git://github.com/wildcat-fp/wildcat.git")
+      developerConnection.set("scm:git:ssh://git@github.com/wildcat-fp/wildcat.git")
+    }
+  }
 }
--- wildcat-asserts/build.gradle.kts
+++ wildcat-asserts/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
   id("common")
+  id("publishing")
 }
 
 dependencies {
